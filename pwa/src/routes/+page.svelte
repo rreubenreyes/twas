74167039666ff90dffa7c1c5f2f0859157a16d6f
Reuben Reyes <reuben.a.reyes@gmail.com>
2024-02-29 21:21:32 -0800

    scaffold webapp

--- pwa/src/routes/+page.svelte
+++ pwa/src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+	<h1>twas</h1>
+	<form id="twas" class="pure-form">
+		<fieldset>
+			<input type="text" />
+			<button type="submit" class="pure-button pure-button-primary">➤</button>
+		</fieldset>
+	</form>
+</main>
+
+<style>
+	main {
+		display: grid;
+		height: 100vh;
+		width: 100vw;
+		justify-items: center;
+		grid-template-rows: 1fr 2fr;
+	}
+
+	form#twas {
+		width: 80vw;
+	}
+	form#twas > fieldset {
+		display: grid;
+		align-self: center;
+
+		grid-template-columns: 3.5fr 1fr;
+		column-gap: 0.5rem;
+	}
+
+	form#twas > fieldset > button,
+	form#twas > fieldset > input[type='text'] {
+		margin: 0;
+	}
+</style>
